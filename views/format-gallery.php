e5cdf040a19cc6c9e2d8b743b7c5dbf9e56e4fc1
mateomtb <mateomtb@gmail.com>
2014-08-27 00:34:24 -0600

    Removed extra div tags and class to simplify styling of input field

--- views/format-gallery.php
+++ views/format-gallery.php
@@ -1,18 +1,16 @@
 <div id="cfpf-format-gallery-preview" class="cf-elm-block cf-elm-block-image" style="display: none;">
 
 	<label><span><?php _e('Gallery Images', 'cf-post-format'); ?></span></label>
+	
 	<div class="cf-elm-container">
-		<div class="cf-elm-block">
-			<div class="cf-elem-inline">
-				<input type="radio" name="_format_gallery_type" id="shortcode" value="shortcode" <?php checked( get_post_meta($post->ID, '_format_gallery_type', true), 'shortcode' ); ?>" id="cfpf-format-gallery-checked-shortcode" tabindex="1" /> Shortcode
-			</div>
-			<div class="cf-elem-inline">
-				<input type="text" name="_format_gallery_preview_shortcode" value="<?php echo esc_attr(get_post_meta($post->ID, '_format_gallery_preview_shortcode', true)); ?>" id="cfpf-format-gallery-preview-shortcode" tabindex="1" />
-			</div>
-			<div>
-				<input type="radio" name="_format_gallery_type" value="attached-images" id="attached-images" <?php checked( get_post_meta($post->ID, '_format_gallery_type', true), 'attached-images' ); ?>" id="cfpf-format-gallery-checked-allimages" tabindex="1" /> All Images
-			</div>
-	</div>
+		<p>
+			<input type="radio" name="_format_gallery_type" id="shortcode" value="shortcode" <?php checked( get_post_meta($post->ID, '_format_gallery_type', true), 'shortcode' ); ?>" id="cfpf-format-gallery-checked-shortcode"  /> Shortcode
+			<span style="display:inline-block;"><input type="text" name="_format_gallery_preview_shortcode" value="<?php echo esc_attr(get_post_meta($post->ID, '_format_gallery_preview_shortcode', true)); ?>" id="cfpf-format-gallery-preview-shortcode" tabindex="1" /></span>
+		</p>
+		<p>
+			<input type="radio" name="_format_gallery_type" value="attached-images" id="attached-images" <?php checked( get_post_meta($post->ID, '_format_gallery_type', true), 'attached-images' ); ?>" id="cfpf-format-gallery-checked-allimages"  /> All Images
+		</p>
+
 
 <?php
 
